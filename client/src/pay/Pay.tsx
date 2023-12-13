6693a55748e6c14b00f47643359a8097a305dcc0
juancodev <montillasanchezjuancarlos@gmail.com>
2023-12-13 01:42:55 -0400

    [🐞BUG] Problem with the render of my pay component

--- client/src/pay/Pay.tsx
+++ client/src/pay/Pay.tsx
@@ -1,21 +1,42 @@
 import { loadStripe } from "@stripe/stripe-js";
-import { Elements, CardElement } from "@stripe/react-stripe-js";
+import {
+  Elements,
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { FormEvent } from "react";
 
 const stripePromise = loadStripe(
   "pk_test_51OLz7PACduFEJ0311jjfGzMg7gZ1jE6mGIAegJ8z5nQB3KAmyaeIrHdCg6WxXmxr6Xh9AokAHbjSrunL85gPN2gP00AYaxNJOi"
 );
 
 const Pay = () => {
-  // TODO: Do the method for payment online - finish ⚠️
-  const handleSubmit = () => {};
+  const stripe = useStripe(); // This is the connection to stripe
+  const elements = useElements();
 
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const payElement = elements?.getElement(PaymentElement);
+    if (!payElement || !stripe) {
+      return;
+    }
+    const result = await stripe.createPaymentMethod({
+      element: payElement,
+    });
+
+    console.log(result);
+  };
+
+  //TODO: fix render of my pay component ⚠️
   return (
     <div className="pay_div_container">
       <div className="pay_section_products"></div>
       <div className="pay_section_form">
         <Elements stripe={stripePromise}>
           <form onSubmit={handleSubmit}>
-            <CardElement />
+            <PaymentElement />
           </form>
         </Elements>
       </div>
